bf1a2866ba9fb3483e245c998797b55713cdb744
Trownt <trsully@gmail.com>
2012-06-13 09:10:46 -0500

    Documentation Cleanup - Missed File

--- SuperAwesomeCode.DataModel/Entities/EntityDataApplicationModule.cs
+++ SuperAwesomeCode.DataModel/Entities/EntityDataApplicationModule.cs
@@ -4,26 +4,20 @@ using Ninject.Modules;
 
 namespace SuperAwesomeCode.DataModel.Entities
 {
-	/// <summary>
-	/// EntityDataApplicationModule class.
-	/// </summary>
-	internal class EntityDataApplicationModule : NinjectModule
+	/// <summary>EntityDataApplicationModule class.</summary>
+	internal sealed class EntityDataApplicationModule : NinjectModule
 	{
 		/// <summary>All of the connection containers.</summary>
 		private IEnumerable<EntityConnectionContainer> _EntityConnectionContainers;
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="EntityDataApplicationModule"/> class.
-		/// </summary>
+		/// <summary>Initializes a new instance of the <see cref="EntityDataApplicationModule"/> class.</summary>
 		/// <param name="entityConnectionContainers">The entity connection containers.</param>
 		internal EntityDataApplicationModule(IEnumerable<EntityConnectionContainer> entityConnectionContainers)
 		{
 			this._EntityConnectionContainers = entityConnectionContainers;
 		}
 
-		/// <summary>
-		/// 	Loads the module into the kernel.
-		/// </summary>
+		/// <summary>Loads the module into the kernel.</summary>
 		public override void Load()
 		{
 			foreach (var container in this._EntityConnectionContainers)
@@ -37,9 +31,7 @@ namespace SuperAwesomeCode.DataModel.Entities
 			this.Bind<IUnitOfWork>().To<EntityUnitOfWork>();
 		}
 
-		/// <summary>
-		/// Obtains the BatchedEntityDataContext.
-		/// </summary>
+		/// <summary>Obtains the BatchedEntityDataContext.</summary>
 		/// <param name="context">The ninject context.</param>
 		/// <returns></returns>
 		private BatchedEntityDataContext ObtainBatchedDataContext(Ninject.Activation.IContext context)
